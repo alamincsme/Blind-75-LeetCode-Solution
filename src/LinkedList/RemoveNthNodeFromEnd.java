88786dc6bf5a6988b3446cbe465a71094c753f42
alamincsme <alamincsme@gmail.com>
2024-01-11 15:19:06 +0600

    Remove Nth Node From End

--- src/LinkedList/RemoveNthNodeFromEnd.java
+++ src/LinkedList/RemoveNthNodeFromEnd.java
@@ -10,49 +10,67 @@ class ListNode {
     ListNode(int val) { this.val = val; }
     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
+
+//good practice
 public class RemoveNthNodeFromEnd {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        if (head == null) {
-            return  null;
-        }else if (head.next == null) {
-            return null;
-        } else {
-            int size = size(head);
-            if (size - n == 0) {
-                head.next = head;
-            } else if (size - n == size) {
-                ListNode curr_node = head;
-                for (int i = 0 ; i < size - 2 ; i++) {
-                    curr_node = curr_node.next ;
-                }
-                curr_node.next = null;
-            } else {
-                ListNode cn = head ;
-                for (int i = 0; i < size - n - 1 ; i++) {
-                    cn = cn.next ;
-                }
-                cn.next = cn.next.next;
-            }
+        ListNode dummy = new ListNode(0, head);
+        ListNode fast = dummy, slow = dummy;
+        while (n-- > 0) {
+            fast = fast.next;
         }
-
-        return  head;
-
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
 
     }
-
-    public int size(ListNode head) {
-        int count = 0 ;
-        ListNode curr_node = head;
-
-        while (curr_node != null) {
-            count ++ ;
-            curr_node = curr_node.next ;
-        }
-        return count ;
-    }
-
-
 }
+
+
+//        if (head == null) {
+//            return  null;
+//        }else if (head.next == null) {
+//            return null;
+//        } else {
+//            int size = size(head);
+//            if (size - n == 0) {
+//                head.next = head;
+//            } else if (size - n == size) {
+//                ListNode curr_node = head;
+//                for (int i = 0 ; i < size - 2 ; i++) {
+//                    curr_node = curr_node.next ;
+//                }
+//                curr_node.next = null;
+//            } else {
+//                ListNode cn = head ;
+//                for (int i = 0; i < size - n - 1 ; i++) {
+//                    cn = cn.next ;
+//                }
+//                cn.next = cn.next.next;
+//            }
+//        }
+//
+//        return  head;
+//
+//
+//    }
+//
+//    public int size(ListNode head) {
+//        int count = 0 ;
+//        ListNode curr_node = head;
+//
+//        while (curr_node != null) {
+//            count ++ ;
+//            curr_node = curr_node.next ;
+//        }
+//        return count ;
+//    }
+
+
+
 
 
 
