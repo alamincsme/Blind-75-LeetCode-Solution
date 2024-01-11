85ef92b50068abc88d77bf100bf0caadd7156f94
alamincsme <alamincsme@gmail.com>
2024-01-11 15:11:19 +0600

    Remove Nth Node End

---
+++ src/LinkedList/RemoveNthNodeFromEnd.java
@@ -0,0 +1,60 @@
+package LinkedList;
+
+
+import java.util.Currency;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+public class RemoveNthNodeFromEnd {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null) {
+            return  null;
+        }else if (head.next == null) {
+            return null;
+        } else {
+            int size = size(head);
+            if (size - n == 0) {
+                head.next = head;
+            } else if (size - n == size) {
+                ListNode curr_node = head;
+                for (int i = 0 ; i < size - 2 ; i++) {
+                    curr_node = curr_node.next ;
+                }
+                curr_node.next = null;
+            } else {
+                ListNode cn = head ;
+                for (int i = 0; i < size - n - 1 ; i++) {
+                    cn = cn.next ;
+                }
+                cn.next = cn.next.next;
+            }
+        }
+
+        return  head;
+
+
+    }
+
+    public int size(ListNode head) {
+        int count = 0 ;
+        ListNode curr_node = head;
+
+        while (curr_node != null) {
+            count ++ ;
+            curr_node = curr_node.next ;
+        }
+        return count ;
+    }
+
+
+}
+
+
+
+
+
