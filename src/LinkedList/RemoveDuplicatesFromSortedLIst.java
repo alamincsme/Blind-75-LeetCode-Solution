a13c7f191914c3432c9f5799300a4944eadb233e
alamincsme <alamincsme@gmail.com>
2024-01-12 12:13:22 +0600

    Remove Duplicates From Sorted List

---
+++ src/LinkedList/RemoveDuplicatesFromSortedLIst.java
@@ -0,0 +1,69 @@
+package LinkedList;
+
+//time complexity O(n^2)
+public class RemoveDuplicatesFromSortedLIst {
+
+    public Node  removeDuplicates(Node head) {
+
+
+        if (head == null || head.next == null) {
+            return head;
+        }
+        int cnt = 0 ;
+        Node curr_node = head;
+        while (curr_node != null) {
+            while (curr_node.next != null && curr_node.val == curr_node.next.val) {
+                curr_node.next = curr_node.next.next;
+            }
+
+            curr_node = curr_node.next;
+        }
+        return head;
+    }
+
+//create node.
+    private Node head;
+
+    public class Node {
+        private int val;
+        private Node next;
+
+        public Node (int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+    //append function.
+    public void  append(int val) {
+        Node node = new Node(val) ;
+        if (head == null) {
+            head = node;
+        } else {
+            node.next = head;
+            head = node;
+        }
+    }
+
+    public void printLL() {
+        removeDuplicates(head);
+        if (head == null) {
+            return;
+        } else {
+            Node curr_node =head;
+            for (; curr_node != null; ) {
+                System.out.println(curr_node.val + " ");
+                curr_node = curr_node.next ;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        RemoveDuplicatesFromSortedLIst rdsl = new RemoveDuplicatesFromSortedLIst();
+        int[] arr = new int [] {4,4,4,1};
+        for (int x : arr) {
+            rdsl.append(x);
+        }
+
+        rdsl.printLL();
+    }
+}
